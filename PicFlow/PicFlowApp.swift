cae03bc981542cd252312137fd82654fe84da6c1
Le Dinh Phuc <phucld@ominext.com>
2025-10-08 14:56:02 +0700

    first commit

---
+++ PicFlow/PicFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicFlowApp.swift
+//  PicFlow
+//
+//  Created by Lauv Edward on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct PicFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
